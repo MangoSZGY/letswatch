a05f412e8236c304fe7e6eec4278eb5cd8a2e9f4
MangoSZGY <2005gyurika@gmail.com>
2026-05-15 14:43:06 +0200

    model: add plot and posterUrl fields to Movie entity

--- src/main/java/hu/letswatch/config/DataLoader.java
+++ src/main/java/hu/letswatch/config/DataLoader.java
@@ -5,6 +5,7 @@ import hu.letswatch.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
 
 @Configuration
 public class DataLoader {
@@ -12,12 +13,17 @@ public class DataLoader {
     @Bean
     CommandLineRunner initDatabase(MovieRepository repository) {
         return args -> {
-            // Itt adjuk hozzá a kezdő adatokat
-            repository.save(new Movie(null, "Shrek", "Andrew Adamson", 2001, null));
-            repository.save(new Movie(null, "Inception", "Christopher Nolan", 2010, null));
-            repository.save(new Movie(null, "Interstellar", "Christopher Nolan", 2014, null));
+            repository.save(new Movie(null, "Shrek", "Andrew Adamson", 2001,
+                    "Egy morcos ogre kalandjai, aki meg akarja menteni a mocsara nyugalmát.",
+                    "https://m.media-amazon.com/images/M/MV5BOGJjMzZmM2UtNjVjYi00ZDEwLThjMTMtNWU2NjgxOWVlY2QyXkEyXkFqcGc@._V1_.jpg",
+                    new ArrayList<>()));
 
-            System.out.println("--- Adatbázis sikeresen feltöltve teszt adatokkal! ---");
+            repository.save(new Movie(null, "Inception", "Christopher Nolan", 2010,
+                    "Tolvajok, akik álmokon keresztül lopnak titkokat.",
+                    "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+                    new ArrayList<>()));
+
+            System.out.println("--- Adatbázis frissítve poszterekkel! ---");
         };
     }
 }
--- src/main/java/hu/letswatch/model/Movie.java
+++ src/main/java/hu/letswatch/model/Movie.java
@@ -1,17 +1,16 @@
 package hu.letswatch.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name = "movies")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Movie {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,6 +18,11 @@ public class Movie {
     private String title;
     private String director;
     private int releaseYear;
+
+    @Column(length = 1000) // Hosszabb szövegnek, hogy a leírás beleférjen
+    private String plot;
+
+    private String posterUrl;
 
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
     private List<Review> reviews;
